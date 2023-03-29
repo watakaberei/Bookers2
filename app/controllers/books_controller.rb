219feda6b20f0166b2a700cb6d13d006d4754d61
EC2 Default User <ec2-user@ip-172-31-40-122.ap-northeast-1.compute.internal>
2023-03-29 13:33:12 +0000

    update 19

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,20 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @user = current_user
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
   def index
     @books = Book.all
+    @users = User.all
     @book = Book.new
     @user = current_user
   end
@@ -38,8 +41,13 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
+      
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,17 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:destroy]
   before_action :correct_user,   only: [:destroy]
-  
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
-    is_matching_login_user
     @user = User.find(params[:id])
   end
 
   def update
-    is_matching_login_user
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
@@ -34,11 +32,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
-  def is_matching_login_user
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to post_images_path
-    end
-  end  
 end
